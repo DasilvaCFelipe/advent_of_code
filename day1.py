3062afeef994317d5800c39b9c828d8c4bf0a806
Felipe da Costa da Silva <dasilva.c.felipe@gmail.com>
2021-12-05 18:48:50 -0300

    Finishing the first day puzzles

--- day1.py
+++ day1.py
@@ -1,14 +1,30 @@
+#first half
+
 increase = lambda x,y: 1 if x > y else 0
 total = int()
 previous = int()
-with open("data/datatest.txt") as f: 
+with open("data/dataday1.txt") as f: 
     for line in f:
         total += increase(int(line), previous)
         previous = int(line)
 
-## minus 1, cause the first step where dont exist a previous measurement count as increased.
+## minus 1, cause in the first step dont exist a previous measurement.
 print(total-1)
 
+#second half
+
+with open("data/dataday1.txt") as f: lst=[int(line.strip()) for line in f]
+
+delta = int()
+total = int()
+previous = sum(lst[delta:delta+3])
+
+for delta in range(1, len(lst)-2, 1):
+    target = sum(lst[delta:delta+3])
+    total += increase(target, previous)
+    previous = target
+
+print(total)
 
 
 
